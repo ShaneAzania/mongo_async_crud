6fa1c75ddcb139140f506c0af2b976841e60f08a
ShaneAzania <87488883+ShaneAzania@users.noreply.github.com>
2023-05-18 09:20:52 -0400

    update

--- middleware/logEvents.js
+++ middleware/logEvents.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const fsPromises = require('fs').promises;
 const path = require('path');
 
-const logEvents = async (message, logName) => {
+const logEvents = async (message, logFileName) => {
     const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
     const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
 
@@ -14,7 +14,7 @@ const logEvents = async (message, logName) => {
             await fsPromises.mkdir(path.join(__dirname, '..', 'logs'));
         }
 
-        await fsPromises.appendFile(path.join(__dirname, '..', 'logs', logName), logItem);
+        await fsPromises.appendFile(path.join(__dirname, '..', 'logs', logFileName), logItem);
     } catch (err) {
         console.log(err);
     }
